30d8477773fc7513d5ab59763dc54133a47653b2
Ashley Angwafor <splashashley@gmail.com>
2022-09-03 13:15:05 +0200

    change size of drawing

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ function drawCircle(x, y){
     ctx.fill();
 }
 
+function drawLine(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(X2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size*2;
+    ctx.stroke();
+}
+
+colorEl.addEventListener('change', (e) =>{
+    color = e.target.value;
+})
+
 canvas.addEventListener('mousedown', (e) =>{
     isPressed = true;
     x = e.offsetX;
@@ -28,6 +41,10 @@ canvas.addEventListener('mousemove', (e) =>{
         const x2 = e.offsetX;
         const y2 = e.offsetY;
         drawCircle(x2,y2);
+        drawLine(x, y, x2, y2);
+
+        x = x2;
+        y = y2;
     }
 })
 
